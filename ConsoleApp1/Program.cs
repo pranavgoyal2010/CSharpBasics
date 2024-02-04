fa10b896db9f85ce76aa6c0417f0e0b268649270
Pranav Goyal <pr945005@dal.ca>
2024-02-04 13:13:28 +0530

    Practice of functions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
@@ -8,6 +9,31 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Console.WriteLine(item);
+            Console.WriteLine(youngestChild(child2: "John", child3: "Nathan", child1: "Victor"));
+            Console.WriteLine(youngestChild("Sam"));
+            Console.WriteLine(multiply(3, 12));
+        }
+
+        static string youngestChild(string child1, string child2, string child3)
+        {
+            //var item = 5;
+            return "the youngest child is " + child3;
+        }
+
+        static string youngestChild(String name = "Harry")
+        {
+            return "the name is " + name;
+        }
+
+        static int multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static double multiply(double x, double y)
+        {
+            return x * y;
         }
     }
 }
