2f50d13b2986fc3b633d9e4e8c48c6db2e1c6d96
Pranav Goyal <pr945005@dal.ca>
2024-02-04 20:31:30 +0530

    practice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,11 +3,38 @@ using System;
 
 namespace ConsoleApp1
 {
+
+    class Example
+    {
+        string name;
+        int age;
+
+        public Example(string name, int age) 
+        { 
+            this.name = name;
+            this.age = age;
+        }
+
+        public Example(Example example) //copy constructor
+        {
+            this.name = example.name;
+            this.age = example.age;
+        }
+
+        public void getData()
+        {
+            Console.WriteLine("Name is {0}", this.name);
+            Console.WriteLine("Age is {0}", this.age);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Example example = new Example("John", 23);
+            example.getData();
+            Example example1 = new Example(example);
+            example1.getData();
         }
     }
 }
