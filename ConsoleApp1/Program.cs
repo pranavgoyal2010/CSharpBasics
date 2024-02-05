a5899e35765d934017064360b69e65f526cc5e29
Pranav Goyal <pr945005@dal.ca>
2024-02-05 10:22:10 +0530

    practice for private constructor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,11 +3,34 @@ using System;
 
 namespace ConsoleApp1
 {
+    
+    class Example
+    {
+        public static int a;
+        private Example() 
+        {
+        }
+        public static int getIncrement()
+        {
+            return ++a;
+        }
+        public static void getTime() 
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+
+    //class Example2 : Example
+    //{
+
+    //}
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Example e = new Example();
+            Example.getTime();
+            Console.WriteLine(Example.getIncrement());
         }
     }
 }
