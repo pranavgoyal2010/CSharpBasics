64ae7bec319cd86c55e50990c949f177e3952f68
Pranav Goyal <pr945005@dal.ca>
2024-02-05 18:41:40 +0530

    practice of usage of constructor in inheritance

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,11 +3,30 @@ using System;
 
 namespace ConsoleApp1
 {
+    class ParentClass
+    {
+        public ParentClass(string message) 
+        {
+            Console.WriteLine(message);
+        }
+        public ParentClass(int i) 
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    class DerivedClass : ParentClass
+    {
+        public DerivedClass() : base("How are you?")
+        {
+            Console.WriteLine("This is derived class constructor");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DerivedClass derivedClass = new DerivedClass();
         }
     }
 }
