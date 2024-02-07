4f765434b940e634d1680ff8c15a2bbcb2a96292
Pranav Goyal <pr945005@dal.ca>
2024-02-07 11:51:25 +0530

    [Pranav] Add : implementation of interface example

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,11 +3,55 @@ using System;
 
 namespace ConsoleApp1
 {
+    public interface Bank
+    {
+        public void deposit (double amount);
+        public void withdraw(double amount);
+
+        public void display();   
+    }
+
+    public class XYZ : Bank
+    {
+        private double currentBalance;
+        public void deposit(double amount)
+        {
+            if(amount<=0)
+                Console.WriteLine("Invalid entry");
+
+            else
+            {
+                currentBalance += amount;
+                Console.WriteLine("Amount {0} added", amount);
+            }
+        }
+        public void withdraw(double amount)
+        {
+            if (amount <= 0)
+                Console.WriteLine("Invalid entry");
+
+            else
+            {
+                currentBalance -= amount;
+                Console.WriteLine("Amount {0} withdrawn", amount);
+            }
+        }
+
+        public void display() 
+        {
+            Console.WriteLine("The current balance: " + currentBalance);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            XYZ John = new XYZ();
+            John.deposit(0);
+            John.deposit(100.34);
+            John.withdraw(100);
+            John.display();
         }
     }
 }
